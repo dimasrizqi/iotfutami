3dc7b7afe0c75c2fc5d33425260b5ebb3b8dc8e2
Administrator <Administrator@CRG-PC-028.stairway.local>
2020-09-24 07:59:25 +0700

    get update

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,22 @@ use Illuminate\Support\Facades\Route;
 */
 // Route::resource('products','ProductController');
 
-Route::get('/', function () {
-    return view('welcome');
+// login
+Route::get('/login', 'otentikasi\OtentikasiController@index' )-> name('login') ;
+Route::post('/login', 'otentikasi\OtentikasiController@login') -> name('login');
+Route::get('/logout', 'otentikasi\OtentikasiController@logout') -> name('logout');
+
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/', function () {
+        return view('/home');
+    });
+    Route::get('/home', function () {
+        return view('/home');
+    });
+    // tambah user
+    Route::get('/tambahuser', 'otentikasi\OtentikasiController@tambah' )-> name('tambah-user') ;
+    Route::post('/tambahuser/simpan', 'otentikasi\OtentikasiController@simpan') -> name('tambah-user-simpan');
+    //profile user
+    Route::get('/profile', 'otentikasi\OtentikasiController@profile' )-> name('profile') ;
+    Route::post('/profile/simpan', 'otentikasi\OtentikasiController@profilesimpan') -> name('profile-user-simpan');
 });
